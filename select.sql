cd0ee60a967d81a476f68ddad973ee46e07d02d5
riyaz-369 <riyazulislam0099@gmail.com>
2025-05-21 13:20:14 +0600

    Scalar and Aggregate Functions in PostgreSQL

--- select.sql
+++ select.sql
@@ -119,4 +119,55 @@ WHERE (
     )
     AND age >= 22
 
+-- to print only first name which starts with R
 SELECT * FROM student WHERE first_name LIKE 'R%'
+
+-- upper case of first name (upper is a scalar func)
+SELECT upper(first_name) as first_name FROM student
+
+--! full name of student (concat is a scalar function)
+SELECT concat(first_name, ' ', last_name) as full_name FROM student
+
+/*
+? Scalar functions
+1. upper()
+2. lower()
+3. concat()
+4. length()
+5. substr()
+6. replace()
+7. trim()
+8. ltrim()
+9. rtrim()
+*/
+
+/*
+? Aggregate functions
+1. count()
+2. sum()
+3. avg()
+4. min()
+5. max()
+6. round()
+7. ceil()
+8. floor()
+9. group by()
+*/
+
+-- to print average age
+SELECT avg(age) FROM student
+
+-- to print maximum age
+SELECT max(age) FROM student
+
+-- to print minimum age
+SELECT min(age) FROM student
+
+-- to print sum of age
+SELECT sum(age) FROM student
+
+-- to print total number of student
+SELECT count(*) as total FROM student
+
+-- to print maximum length of first name
+SELECT max(length(first_name)) FROM student
