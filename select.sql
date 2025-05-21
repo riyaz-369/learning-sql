0fca60f9f50cdc76f81cce3dbee10d53790492c3
riyaz-369 <riyazulislam0099@gmail.com>
2025-05-21 12:44:42 +0600

    SELECT Queries: Column Aliasing and Result Ordering in PostgreSQL

---
+++ select.sql
@@ -0,0 +1,103 @@
+CREATE Table Student (
+    student_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    age INT,
+    grade CHAR(2),
+    course VARCHAR(50),
+    email VARCHAR(50),
+    dob DATE,
+    blood_group VARCHAR(5),
+    country VARCHAR(50)
+)
+
+INSERT INTO
+    student (
+        first_name,
+        last_name,
+        age,
+        grade,
+        course,
+        email,
+        dob,
+        blood_group,
+        country
+    )
+VALUES (
+        'Rakesh',
+        'Kumar',
+        22,
+        'A',
+        'B.Tech',
+        '3dL1e@example.com',
+        '1999-01-01',
+        'A+',
+        'India'
+    ),
+    (
+        'John',
+        'Doe',
+        20,
+        'B',
+        'B.Tech',
+        '7bV0t@example.com',
+        '1999-01-01',
+        'A+',
+        'USA'
+    ),
+    (
+        'Rohan',
+        'Kumar',
+        22,
+        'A',
+        'B.Tech',
+        '3dL1e@example.com',
+        '1999-01-01',
+        'A+',
+        'India'
+    ),
+    (
+        'Abdul',
+        'Kalam',
+        22,
+        'A',
+        'B.Tech',
+        '3dL1e@example.com',
+        '1999-01-01',
+        'A+',
+        'Bangladesh'
+    ),
+    (
+        'Hamid',
+        'Khan',
+        22,
+        'A',
+        'B.Tech',
+        '3dL1e@example.com',
+        '1999-01-01',
+        'A+',
+        'Pakistan'
+    ),
+    (
+        'Tom',
+        'Hanks',
+        22,
+        'A',
+        'B.Tech',
+        '3dL1e@example.com',
+        '1999-01-01',
+        'A+',
+        'USA'
+    )
+
+-- to print all data
+SELECT * FROM student
+
+-- to print only first name and email
+SELECT first_name, email FROM student
+
+-- to print only email and alias
+SELECT email as student_email FROM student
+
+-- sort data in a specific order
+SELECT * FROM student ORDER BY first_name ASC
